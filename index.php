a2632d942589cae1e7686946ac11e8585f5c8a4f
alex1707-24 <159423591+alex1707-24@users.noreply.github.com>
2024-12-25 19:11:16 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style>
+		html, body{
+			height: 100%;
+		}
+	</style>
+	<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">													
+</head>
+<body>
+	<div class="col-12  p-3" style="background-color: #171A21;">
+		<div class="col-8 mx-auto" >
+			<div class="row">
+				<div class="col-3" >
+					<a href="index.html">
+						<img src="logo.png" class="w-100">
+					</a>
+				</div>
+				<div class="col-9 pt-4" >
+					<a href="" class="text-light ms-4 text-uppercase">Магазин</a>
+					<a href="" class="text-light ms-4 text-uppercase">Сообщество</a>
+					<a href="" class="text-light ms-4 text-uppercase">O STEAM</a>
+					<a href="" class="text-light ms-4 text-uppercase">Поддержка</a>	
+					<a href="admin.php">добавление игры</a>				
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<div class="col-12" style="background-color: #212429; height: 100%;">
+		<div class="col-8 mx-auto pt-5">
+			<div class="bg-danger col-8 mx-auto" style="height: 900px;">
+				<div class="d-flex">
+					<img src="аватарка.jpg"style="height: 100px;width: 100px;">
+					<p class="fs-4">Списак желаемого</p>
+				</div>
+				<div class="d-flex">
+					<div class="col-6">
+						<input class="bg-red"></input>
+					</div>	
+					<div class="d-flex bg-light col-2" style="height: 50px;">
+						<p class="fs-4">настройки</p>
+						<button>\/</button>
+					</div>
+					<div class="d-flex bg-light col-4" style="height: 50px;">
+						<p class="fs-4 text-primary">Сортировывать по: </p>
+						<p class="fs-4">вашему предпочтению</p>
+						<button>\/</button>
+					</div>
+				</div>
+				<div style="height:150px;">
+					<div class="d-flex">
+						<img src="Снимок экрана_30-10-2024_19723_media.discordapp.net.jpeg">
+						<div>
+							<div class="d-flex ms-2">
+								<div class="col-7">
+									<img src="rust-file-53660cf1.jpeg.webp"class="col-12"style="height: 150px;">
+								</div>
+								<div>
+									<p class="fs-3">RAST</p>
+								</div>
+								<div class="d-flex">
+									<p>510.40 руб</p>
+									<button class="btn btn-primary btn-sm"style="height: 30px;">В корзину</button>
+								</div>							
+							</div>
+						</div>						
+					</div>
+					<div class="d-flex">
+					<img src="Снимок экрана_30-10-2024_19723_media.discordapp.net.jpeg">
+						<div>
+							<div class="d-flex ms-2">
+								<div  class="col-7">
+									<img src="9ba1caeb9e9e8aaee648826e68eef6f8b876ef213db346d8f402cd159d34308d.avif"class="col-12"style="height: 150px;">
+								</div>
+								<div>
+									<p class="fs-3">SNOWRUNNER</p>
+								</div>
+								<div class="d-flex">
+									<p>?</p>
+									<button class="btn btn-primary btn-sm"style="height: 30px;">В корзину</button>
+								</div>							
+							</div>
+						</div>
+					</div>
+					<div class="d-flex">
+						<img src="Снимок экрана_30-10-2024_19723_media.discordapp.net.jpeg">
+						<div>
+							<div class="d-flex ms-2">
+								<div class="col-7">
+									<img src="article-gta-5-banner.jpg"class="col-12"style="height: 150px;">
+								</div>
+								<div>
+									<p class="fs-3">GTA V</p>
+								</div>
+								<div class="d-flex">
+									<p>?</p>
+									<button class="btn btn-primary btn-sm"style="height: 30px;">В корзину</button>
+								</div>							
+							</div>
+						</div>						
+					</div>
+				</div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
